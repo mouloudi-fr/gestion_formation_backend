e4d111d58039fafc895d386a6384679eea497af6
mouloudi-fr <abdellatif.mo@gmail.com>
2023-09-29 19:04:47 +0200

    first commit

---
+++ src/main/java/fr/gestionformation/GestionFormationBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.gestionformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFormationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFormationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/gestionformation/GestionFormationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gestionformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFormationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
