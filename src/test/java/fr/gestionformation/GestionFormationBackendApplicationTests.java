063524709de83acb23169bfcfe954b91d3bf2519
mouloudi-fr <abdellatif.mo@gmail.com>
2023-10-04 01:35:39 +0200

    fix erro context config

--- src/test/java/fr/gestionformation/GestionFormationBackendApplicationTests.java
+++ src/test/java/fr/gestionformation/GestionFormationBackendApplicationTests.java
@@ -3,9 +3,11 @@ package fr.gestionformation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(classes=GestionFormationBackendApplicationTests.class)
 @ActiveProfiles("test")
+@ContextConfiguration
 class GestionFormationBackendApplicationTests {
 
 	@Test
